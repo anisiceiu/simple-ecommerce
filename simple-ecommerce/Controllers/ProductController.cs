6f7ec3e5864bf393732dc9550fca85b2d1b5c5f0
anis <anisictiu@gmail.com>
2026-01-23 15:22:43 +0600

    works update

---
+++ simple-ecommerce/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using ECommerce.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace simple_ecommerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+          _productService = productService;      
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _productService.GetProductsAsync();    
+
+            return Ok(products);
+        }
+    }
+}
